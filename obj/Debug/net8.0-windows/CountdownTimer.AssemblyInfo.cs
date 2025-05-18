4113135b3fa39adc943944540f36fafbb0f0aebf
ZhengTingYi <s1131335@pu.edu.tw>
2025-05-18 21:40:17 +0800

    Update CountdownTimer.AssemblyInfo.cs

--- obj/Debug/net8.0-windows/CountdownTimer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CountdownTimer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CountdownTimer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2812b8acd7394f43361ebcd3f2944b126327d78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CountdownTimer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CountdownTimer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
